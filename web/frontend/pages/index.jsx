8f077aaa2d82ff83d14f321706210fcfa47ba9d7
Jean Manzo <jdevmanzo@gmail.com>
2022-10-04 17:27:18 -0500

    list view connected to display real saved QR codes

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -7,6 +7,8 @@ import {
   SkeletonBodyText
 } from "@shopify/polaris";
 
+import { useAppQuery } from "../hooks";
+
 import { QRCodeIndex } from "../components";
 
 export default function HomePage() {
@@ -16,40 +18,20 @@ export default function HomePage() {
    */
   const navigate = useNavigate();
 
-  const isLoading = false;
-  const isRefetching = false;
-  const QRCodes = [
-    {
-      createdAt: "2022-06-13",
-      destination: "checkout",
-      title: "My first QR code",
-      id: 1,
-      discountCode: "SUMMERDISCOUNT",
-      product: {
-        title: "Faded t-shirt",
-      }
-    },
-    {
-      createdAt: "2022-06-13",
-      destination: "product",
-      title: "My second QR code",
-      id: 2,
-      discountCode: "WINTERDISCOUNT",
-      product: {
-        title: "Cozy parka",
-      }
-    },
-    {
-      createdAt: "2022-06-13",
-      destination: "product",
-      title: "QR code for deleted product",
-      id: 3,
-      product: {
-        title: "Deleted product",
-      }
-    },
-  ];
-  
+  /* useAppQuery wraps react-query and the App Bridge authenticatedFetch function */
+  const {
+    data: QRCodes,
+    isLoading,
+    /*
+      react-query provides stale-while-revalidate caching.
+      By passing isRefetching to Index Tables we can show stale data and a loading state.
+      Once the query refetches, IndexTable updates and the loading state is removed.
+      This ensures a performant UX.
+    */
+    isRefetching,
+  } = useAppQuery({
+    url: "/api/qrcodes",
+  });
 
   const qrCodesMarkup = QRCodes?.length ? (
     <QRCodeIndex QRCodes={QRCodes} loading={isRefetching} />
