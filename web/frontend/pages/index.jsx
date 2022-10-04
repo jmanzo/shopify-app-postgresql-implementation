55f27350ce412571288cb7eab69ea2a307de7931
Jean Manzo <jdevmanzo@gmail.com>
2022-10-04 16:31:15 -0500

    added a list component to the homepage

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -7,6 +7,8 @@ import {
   SkeletonBodyText
 } from "@shopify/polaris";
 
+import { QRCodeIndex } from "../components";
+
 export default function HomePage() {
   /**
    * Added useNavigate hook to set up the navigate function.
@@ -16,7 +18,42 @@ export default function HomePage() {
 
   const isLoading = false;
   const isRefetching = false;
-  const QRCodes = [];
+  const QRCodes = [
+    {
+      createdAt: "2022-06-13",
+      destination: "checkout",
+      title: "My first QR code",
+      id: 1,
+      discountCode: "SUMMERDISCOUNT",
+      product: {
+        title: "Faded t-shirt",
+      }
+    },
+    {
+      createdAt: "2022-06-13",
+      destination: "product",
+      title: "My second QR code",
+      id: 2,
+      discountCode: "WINTERDISCOUNT",
+      product: {
+        title: "Cozy parka",
+      }
+    },
+    {
+      createdAt: "2022-06-13",
+      destination: "product",
+      title: "QR code for deleted product",
+      id: 3,
+      product: {
+        title: "Deleted product",
+      }
+    },
+  ];
+  
+
+  const qrCodesMarkup = QRCodes?.length ? (
+    <QRCodeIndex QRCodes={QRCodes} loading={isRefetching} />
+  ) : null;
 
   const loadingMarkup = isLoading ? (
     <Card sectioned>
@@ -44,7 +81,7 @@ export default function HomePage() {
    * and include the empty state contents set above.
    */
   return (
-    <Page>
+    <Page fullWidth={!!qrCodesMarkup}>
       <TitleBar 
         title="QR codes"
         primaryAction={{
@@ -55,6 +92,7 @@ export default function HomePage() {
       <Layout>
         <Layout.Section>
           {loadingMarkup}
+          {qrCodesMarkup}
           {emptyStateMarkup}
         </Layout.Section>
       </Layout>
